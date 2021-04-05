957df5f84e0575f7763584112f4a9ace535115a2
Molly <mollymccollumwx@gmail.com>
2021-04-05 09:44:44 -0400

    added several new pages, basic layout and added them to the app.js with a route

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem'
+import "./Cards.css"
 
 function Cards() {
     return (
@@ -8,7 +9,18 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                        src="images/Endor.png" text="Explore the nature reserve on the forest moon of Endor" label="Adventure" path="/services"/>
+                        <CardItem 
+                        src="images/tatooine.jpeg" text="Enjoy dual sunsets on Tatooine from Jabbas Palace" label="Luxury" path="/services"/>
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                        src="images/Endor.png" text="Explore the nature reserve on the forest moon of Endor" label="Adventure" path="/services"/>
+                        <CardItem 
+                        src="images/tatooine.jpeg" text="Enjoy dual sunsets on Tatooine from Jabbas Palace" label="Luxury" path="/services"/>
+                        <CardItem 
+                        src="images/tatooine.jpeg" text="Enjoy dual sunsets on Tatooine from Jabbas Palace" label="Luxury" path="/services"/>
                     </ul>
                 </div>
             </div>
---
+++ src/components/pages/Signup.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../../App.css"
+
+
+
+export default function Signup() {
+    return <h1 className="signup">SIGNUP</h1>
+}
