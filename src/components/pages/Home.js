44f499d3099fc997f87969112f6aa06a4aebd957
Molly <mollymccollumwx@gmail.com>
2021-04-05 09:19:09 -0400

    card components plus new cards styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import '../../App.css'
+import Cards from '../Cards'
 import Hero from "../Hero"
 
 function Home() {
     return(
         <>
         <Hero />
+        <Cards />
         </>
     )
 }
